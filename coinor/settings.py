bdaf6592d602c2a65c502b49ddf8faf1d3e14434
Andres Vazquez <andres@data99.com.ar>
2016-07-09 19:05:12 -0300

    try databse

--- coinor/settings.py
+++ coinor/settings.py
@@ -75,16 +75,9 @@ WSGI_APPLICATION = 'coinor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'localhost',
-        'NAME': 'coinor',
-        'USER': 'coinor',
-        'PASSWORD': 'coinor',
-        'PORT': '5432'
-    }
-}
+# DATABASE_URL = 'postgres://coinor:coinor@localhost:5432/coinor'
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
