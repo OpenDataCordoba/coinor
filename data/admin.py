3fb5442029cfa97cd1590ab6ded4de4d126f3c29
Gaston Avila <avila.gas@gmail.com>
2016-07-26 00:00:46 -0300

    Add django-import-export to Persona

--- data/admin.py
+++ data/admin.py
@@ -6,6 +6,21 @@ admin.site.register(Pais)
 admin.site.register(Ciudad)
 admin.site.register(TipoPersona)
 admin.site.register(UniqueId)
-admin.site.register(Persona)
+
 admin.site.register(TipoRelacion)
 admin.site.register(Relacion)
+
+
+from import_export import resources
+
+class PersonaResource(resources.ModelResource):
+
+    class Meta:
+        model = Persona
+
+from import_export.admin import ImportExportModelAdmin
+
+class PersonaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Persona, PersonaAdmin)
